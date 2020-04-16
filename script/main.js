0d7827d5ff7e283328bffec26dbf5ddf147d63f2
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-16 15:43:36 +0200

    html css completed

--- script/main.js
+++ script/main.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     console.log('main.js is working')
     console.log($)
 
-
+    const input = $('#new-input')
+    const input_instrcution = $('.input-box .instruction')
    
+    
 })
